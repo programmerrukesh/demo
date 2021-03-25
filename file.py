063bc3d4fce7286d11aa03da367e126d5a7a7021
rukesh.shrestha <rukesh.shrestha11@gmail.com>
2021-03-25 08:38:18 +0545

    python file added

---
+++ file.py
@@ -0,0 +1,31 @@
+def summ(num1,num2):
+    return num1+num2
+
+print(summ(5,6))
+
+
+def palendrime(something):
+    reverse = something[::-1]
+    if something == reverse:
+        print("Hey yoyo!")
+    else:
+        print("nothing")
+        
+palendrime("Mama")
+
+
+def counter():
+    sentence = input("Please enter any sentence")
+    counter = dict()
+    for x in sentence:
+        if x not in counter:
+            counter[x] + 1
+        else:
+            counter[x] +=1
+counter()
+
+def substract(num1,num2):
+    return num1-num2
+substract(10,6)
+
+print("Hey")
